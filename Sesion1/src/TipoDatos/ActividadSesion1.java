aa18035e697300384b3ba376e6f83d67677f335b
ru3enaa1 <133252166+ru3enaa1@users.noreply.github.com>
2023-06-24 16:56:47 -0500

    Add files via upload
    Creacion de proyecto con su paquete y clase, mostrar los tipos de datos en la terminal

---
+++ Sesion1/src/TipoDatos/ActividadSesion1.java
@@ -0,0 +1,48 @@
+package TipoDatos;
+
+public class ActividadSesion1 {
+
+    public static void main(String[] args) {
+
+        //1. Numericos
+
+        //1.1. Enteros
+        
+        Byte num1 = 6;
+        System.out.println(num1);
+
+        short num2 = 10;
+        System.out.println(num2);
+
+        int num3 = 45;
+        System.out.println(num3);
+
+        long num4 = 78;
+        System.out.println(num4);
+
+        //1.2. Decimales
+        
+        float num5 = 12.5f;
+        System.out.println(num5);
+
+        double num6 = 15.0d;
+        System.out.println(num6);
+
+        //2. Booleanos
+
+        boolean var1 = true;
+        System.out.println(var1);
+
+        boolean var2 = false;
+        System.out.println(var2);
+
+        //3. Texto
+
+        char letra = 'a';
+        System.out.println(letra);
+ 
+        String texto = "Hola Mundo";
+        System.out.println(texto);
+    }
+
+}
